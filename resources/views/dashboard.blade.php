19f94acad61cf44fb85c7fd87dff08c01bf810b0
Pablo Fajardo <pfajardodiaz17@gmail.com>
2021-09-17 17:13:01 -0500

    Texto en los contenedores

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,38 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    Welcome
+                    Que es Web Scraping?
+                </div>
+                <div class="p-6 bg-white border-b border-gray-200">
+                Los motores de búsqueda, como Google, utilizan desde hace tiempo los denominados rastreadores
+                web o crawlers, que exploran Internet en busca de términos definidos por el usuario.
+                Los rastreadores son tipos especiales de bots, que visitan una página web tras otra para
+                generar asociaciones con términos de búsqueda y categorizarlos. El primer rastreador web se
+                creó ya en 1993, cuando se presentó el primer motor de búsqueda: Jumpstation.
+                </div>
+            </div>
+
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    Que es la Autenticación?
+                </div>
+                <div class="p-6 bg-white border-b border-gray-200">
+                Los motores de búsqueda, como Google, utilizan desde hace tiempo los denominados rastreadores
+                web o crawlers, que exploran Internet en busca de términos definidos por el usuario.
+                Los rastreadores son tipos especiales de bots, que visitan una página web tras otra para
+                generar asociaciones con términos de búsqueda y categorizarlos. El primer rastreador web se
+                creó ya en 1993, cuando se presentó el primer motor de búsqueda: Jumpstation.
+                </div>
+            </div>
+
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    Las tecnologias que he utilizado
+                </div>
+                <div class="p-6 bg-white border-b border-gray-200">
+                <p>Laravel</p>
+                <p>Laravel Breeze</p>
+                <p>Tailwind.css</p>
                 </div>
             </div>
         </div>
