20645e4f388bd0bde03468f9693e1e755d1f7bb8
Pablo Fajardo <pfajardodiaz17@gmail.com>
2021-09-18 12:03:56 -0500

    Barra de Debug

--- config/app.php
+++ config/app.php
@@ -166,15 +166,19 @@ return [
          * Package Service Providers...
          */
 
+        
+        
         /*
-         * Application Service Providers...
-         */
+        * Application Service Providers...
+        */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        
 
+        Barryvdh\Debugbar\ServiceProvider::class,
     ],
 
     /*
@@ -228,6 +232,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
+
 
     ],
 
